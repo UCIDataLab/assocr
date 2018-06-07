e75b6cf6d4409a89e9dda2311dbf8ecbdaaf9bbc
galbraic <galbraic@uci.edu>
2018-06-07 12:57:01 -0700

    function to sessionize data

--- R/markedPP_funcs.R
+++ R/markedPP_funcs.R
@@ -40,6 +40,78 @@ format_data <- function(data, id.sum, dev=FALSE, low.bds){
 }
 
 ###################################################################################
+#' Group pairs of event series data into sessions.
+#'
+#' @param data Data.frame of event data, \code{<id, t, m>}
+#' @param thres Number of inactive minutes to consider a new session start
+#' @param timeScale Scale of time measurements of data; one of
+#'   \code{c("s", "min", "hr", "day")}
+#' @return List of data.frames:
+#'   \itemize{
+#'     \item \code{data}: sessionized data, \code{<id, m, sid, t>}
+#'     \item \code{sessions}: summary of sessionized data, \code{<id, m, sid, n, t>}
+#'   }
+sessionize_data <- function(data, thres = 10, timeScale = "min"){
+  # convert event times to minutes
+  if (timeScale == "s"){
+    data$t.min <- data$t / 60
+  } else if (timeScale == "min") {
+    data$t.min <- data$t
+  } else if (timeScale == "hr"){
+    data$t.min <- data$t * 60
+  } else if (timeScale == "day"){
+    data$t.min <- data$t * 24 * 60
+  } else {
+    stop("Invalid value for timeScale; enter one of (\"s\", \"min\", \"hr\", \"day\")")
+  }
+
+  ids <- unique(data$id)  # get unique ids from the data
+  for (i in ids) {  # sessionize data for all users one at a time
+    tmp.1 <- .find_sessions(data[data$id == i & data$m == 1, ])  # user i mark 1
+    tmp.1$sessions$m <- rep(1, nrow(tmp.1$sessions))
+    tmp.2 <- .find_sessions(data[data$id == i & data$m == 2, ])  # user i mark 2
+    tmp.2$sessions$m <- rep(2, nrow(tmp.2$sessions))
+    if (i == ids[1]) {  # if its the first user, make data.frames to export
+      dat <- rbind(tmp.1$data, tmp.2$data)
+      ses <- rbind(tmp.1$sessions, tmp.2$sessions)
+    } else {  # data.frames already made, concatenate current user's data
+      dat <- rbind(dat, rbind(tmp.1$data, tmp.2$data))
+      ses <- rbind(ses, rbind(tmp.1$sessions, tmp.2$sessions))
+    }
+  }
+  dat <- dat[order(dat$t), ]
+  row.names(dat) <- 1:nrow(dat)
+  ses <- ses[order(ses$t), ]
+  row.names(ses) <- 1:nrow(ses)
+  return ( list(data = dat[, c("id", "m", "sid", "t")],
+                sessions = ses[, c("id", "m", "sid", "n", "t")]) )
+}
+
+###################################################################################
+#' Group single event series (i.e., one user & one type of event) into sessions.
+#'
+#' @param data Data.frame of event data of same mark for one user, \code{<id, t, t.min>}
+#' @param thres Number of inactive minutes to consider a new session start
+#' @param timeScale Scale of time measurements of data; one of
+#'   \code{c("s", "min", "hr", "day")}
+#' @return List of data.frames:
+#'   \itemize{
+#'     \item \code{data}: sessionized data, \code{<t, id, t.min, diff, sid>}
+#'     \item \code{sessions}: summary of sessions, \code{<id, t, t.min, sid, n>}
+#'   }
+.find_sessions <- function(data, thres = 10, timeScale = "min"){
+  data$diff <- c(0, diff(data$t.min))  # get times between events
+  row.names(data) <- 1:nrow(data)
+  data.sessions <- data[data$diff == 0 | data$diff >= thres, c("id", "t", "t.min")]  # find session starts
+  data.sessions$sid <- 1:nrow(data.sessions)  # number sessions
+  data.ses.len <- diff(as.numeric(row.names(data.sessions)))
+  data.ses.len <- c(data.ses.len, nrow(data) - sum(data.ses.len))
+  data.sessions$n <- data.ses.len  # n events in each session
+  data$sid <- rep(data.sessions$sid, data.ses.len)  # label sessions in raw data
+  return ( list(data = data, sessions = data.sessions) )
+}
+
+###################################################################################
 #' Simulate a pair of event time series.
 #'
 #' @param lambda Intensity of first process (i.e. of Mark 1/A).
@@ -104,8 +176,8 @@ sim_markedpp <- function(lambda, W, nSamp, indep = FALSE, p, normal = TRUE, sigm
 #' @param x Data.frame of \code{<t, m>} single pair of event series.
 #' @param W Observation window \code{c(low, high)}, equivalent to \code{xlim}.
 #' @param c Numeric cex value for axis & legend magnification; \code{default == 1}
-#' @param lbl Vector of labels for the legend corresponding to marks 1/A and 2/B, respectively;
-#'   \code{default == c("Mark 1", "Mark 2")}
+#' @param lbl Vector of labels for the legend corresponding to marks 1/A and 2/B,
+#'   respectively; \code{default == c("Mark 1", "Mark 2")}
 #' @param title Main title of plot; \code{default == ""}
 #' @param xlab X-axis label; \code{default == "Time"}
 plot_markedpp <- function(x, W, c=1, lbl=c("Mark 1", "Mark 2"), title="", xlab="Time"){
